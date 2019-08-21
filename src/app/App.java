09f839d76422a10b8f7745b2689b98489de83618
Nitish-Yadav <nitish_yadav@persistent.com>
2019-08-21 14:34:47 +0530

    Fixed Hello to Awesome

--- src/app/App.java
+++ src/app/App.java
@@ -1,9 +1,10 @@
 package app;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello Java");
-        System.out.println("Hello Game");
-        System.out.println("Hello Nikhil");
+        System.out.println("java isn't awesome");
+        System.out.println("Game is awersome");
+        System.out.println("Nikhil is awesome");
+        System.out.println("We are all awesome!");
 
     }
 }
