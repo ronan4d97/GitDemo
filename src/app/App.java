d216b1437ff2cdf64963cb19b6fa510c77294de0
Nitish-Yadav <nitish_yadav@persistent.com>
2019-08-21 14:38:30 +0530

    Added one more line

--- src/app/App.java
+++ src/app/App.java
@@ -1,9 +1,11 @@
 package app;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello Java");
         System.out.println("Hello Game");
         System.out.println("Hello Nikhil");
+        System.out.println("Nikhil is bored with training");
 
     }
 }
