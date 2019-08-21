e0284140fbb056e7a5b51ee499667859987fb79d
Nitish-Yadav <nitish_yadav@persistent.com>
2019-08-21 14:52:13 +0530

    Added everything

--- src/app/App.java
+++ src/app/App.java
@@ -2,14 +2,16 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("java isn't awesome");
-        System.out.println("Game is awersome");
-        System.out.println("Nikhil is awesome");
-        System.out.println("We are all awesome!");
         System.out.println("Hello Java");
         System.out.println("Hello Game");
         System.out.println("Hello Nikhil");
         System.out.println("Nikhil is bored with training");
+        System.out.println("java isn't awesome");
+        System.out.println("Game is awersome");
+        System.out.println("Nikhil is awesome");
+        System.out.println("We are all awesome!");
+        
+
 
     }
 }
